2a2dcb6cea256725131089eb2b76b5f33c6a296b
unknown <lamlaurendy@gmail.com>
2019-09-27 22:43:49 -0400

    version-4.0 adding background

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,28 +7,31 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import wallpaper from "../images/hill.svg"
 
 import "./layout.scss"
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `8rem 1.0875rem 1.45rem`,
-        }}
-      >
-        <h1 className="title">Under Construction</h1>
-        <main>{children}</main>
-        {/* <footer>
-          © {new Date().getFullYear()}, Built with love
-          {` `}
-          <a href="https://github.com/ghiobi/me">Laurendy Lam</a>
-        </footer> */}
+    <div>
+      <section className="section" style={{ paddingTop: '8rem', paddingBottom: '1rem' }}>
+        <div className="container">
+          <h1 className="title">Under Construction</h1>
+        </div>
+      </section>
+      <main className="section" style={{ paddingTop: 0 }}>
+        <div className="container">
+          {children}
+        </div>
+      </main>
+      <div className="wallpaper-container">
+        <div className="wallpaper">
+          <div>
+            <img className="wallpaper-image" src={wallpaper} />
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,7 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness. Go back <Link to="/">home</Link>.</p>
+    <p>Sorry! Page not found... You just hit a route that doesn&#39;t exist... the sadness. Go back <Link to="/">home</Link>.</p>
   </Layout>
 )
 
